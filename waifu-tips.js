0724b9d843384f9ba8317964a570d32a7da3e575
rhyryy <64307480+rhyryy@users.noreply.github.com>
2021-08-05 20:56:37 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -32,6 +32,13 @@ function loadWidget(config) {
 			<div id="waifu-tips"></div>
 			<canvas id="live2d" width="800" height="800"></canvas>
 			<div id="waifu-tool">
+			<span class="fa fa-lg fa-comment"></span>
+				<span class="fa fa-lg fa-paper-plane" style="display: none;"></span>
+				<span class="fa fa-lg fa-user-circle" style="display: none;"></span>
+				<span class="fa fa-lg fa-street-view" style="display: none;"></span>
+				<span class="fa fa-lg fa-camera-retro" style="display: none;"></span>
+				<span class="fa fa-lg fa-info-circle" style="display: none;"></span>
+				<span class="fa fa-lg fa-times" style="display: none;"></span>
 			</div>
 		</div>`);
 		}
